5f2ac2ea44ab9d8e718695b1b8f23ec6c9abe991
MorbidCastillian69 <shweta.prasad_ug21@ashoka.edu.in>
2020-06-22 23:41:59 +0530

    added stuff

--- raw_abf_plot.py
+++ raw_abf_plot.py
@@ -1,8 +1,8 @@
 import pyabf
 import matplotlib.pyplot as plt
 
-f = pyabf.ABF("<filename>.abf")							#load file
-fig = plt.figure(figsize = (8,6))						#to plot
+f = pyabf.ABF("24.abf")							#load file
+fig = plt.figure(figsize = (8,6))					#to plot
 
 #plotting multichannel abfs: 
 #here, we have 2 channels (indexed from 0)
@@ -15,4 +15,4 @@ plt.plot(f.sweepX, f.sweepY, label = "Channel 2 [V]")
 plt.ylabel(f.sweepLabelY)
 plt.xlabel(f.sweepLabelX)
 plt.legend()
-plt.show()												#shows final result
+plt.show()								#shows final result
