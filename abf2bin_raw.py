dc39d9d46b7a349af71e01788c45b38591a99fbf
MorbidCastillian69 <shweta.prasad_ug21@ashoka.edu.in>
2020-08-05 15:22:04 +0530

    added .abf to .raw conversion

---
+++ abf2bin_raw.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import neo
+
+for i in range(1, 27):
+	#names of i/o files
+	f_in = str(i)+".abf"
+	f_out = str(i)+".raw"
+
+	reader = neo.io.AxonIO(filename = f_in)
+	writer = neo.io.RawBinarySignalIO(f_out, dtype = 'float32', sampling_rate = 10000.0, nb_channel = 2)
+
+	block = reader.read_block(lazy = False)
+	writer.write(block)
+	writer.header = reader.header
